9fd25a6233b0b38063619e53e2220f69961c1050
gang.lin <gang.lin@upai.com>
2016-01-21 15:04:37 +0800

    Changes to be committed: 	new file:   UpYunSDK2.0/UpYunSDK2.0/Utils/UPMutUpload/UMUUploaderManager.h 	new file:   UpYunSDK2.0/UpYunSDK2.0/Utils/UPMutUpload/UMUUploaderManager.m
     Changes not staged for commit:
    	deleted:    UpYunSDK2.0/UpYunSDK2.0/Utils/UMUUpload/UMUUploaderManager.h
    	deleted:    UpYunSDK2.0/UpYunSDK2.0/Utils/UMUUpload/UMUUploaderManager.m
    	modified:   UpYunSDK2.0Demo/UpYunSDK2.0Demo.xcodeproj/project.pbxproj
    	modified:   UpYunSDK2.0Demo/UpYunSDK2.0Demo.xcodeproj/project.xcworkspace/xcuserdata/youpaiyun-lin.xcuserdatad/UserInterfaceState.xcuserstate

---
+++ UpYunSDK2.0/UpYunSDK2.0/Utils/UPMutUpload/UMUUploaderManager.h
@@ -0,0 +1,85 @@
+//
+//  UMUUploaderManager.h
+//  UpYunMultipartUploadSDK
+//
+//  Created by Jack Zhou on 6/10/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UMUUploaderManager : NSObject
+
+#pragma mark - setup Methods
+
+/**
+ *  设置授权时间长度（秒）默认为60秒
+ */
++ (void)setValidTimeSpan:(NSInteger)validTimeSpan;
+
+/**
+ *  设置服务器地址 默认 @"http://m0.api.upyun.com/"
+ *
+ *  @param server 服务器地址
+ */
++ (void)setServer:(NSString *)server;
+
+
+/**
+ *  设置请求重试次数 默认为:3
+ *
+ *  @param 重试次数
+ */
++ (void)setMaxRetryCount:(NSInteger)retryCount;
+
+#pragma mark - init Method
+
+/**
+ *  返回UMUUploaderManager 单例
+ *
+ *  @return bucket 空间 bucket
+ *  @return UMUUploaderManager 实例
+ */
++ (instancetype)managerWithBucket:(NSString *)bucket;
+
+#pragma mark - Method
+
+/**
+ *  获取文件元信息 计算policy、signature需要此信息
+ *
+ *  @param fileData 文件数据
+ *
+ *  @return 获取文件元信息 字典
+ */
++ (NSDictionary *)fetchFileInfoDictionaryWith:(NSData *)fileData;
+
+/**
+ *  上传文件
+ *
+ *  @param fileData                       文件数据
+ *  @param policy                         策略信息
+ *  @param signature                      签名
+ *  @param progressBlock                  进度回调
+ *  @param completeBlock                  结束回调:
+ *                                          当completed
+ *                                          为  YES 上传成功，可以从result中获取返回信息，
+ *                                          为  NO  上传失败，可以从error 获取失败信息
+ *
+ *  @return UMUUploaderOperation
+ */
+- (NSMutableArray *)uploadWithFile:(NSData *)fileData
+                                  policy:(NSString *)policy
+                               signature:(NSString *)signature
+                           progressBlock:(void (^)(float percent,
+                                                   long long requestDidSendBytes))progressBlock
+                           completeBlock:(void (^)(NSError * error,
+                                                   NSDictionary * result,
+                                                   BOOL completed))completeBlock;
+
+/**
+ *  取消所有请求
+ */
++ (void)cancelAllOperations;
+
+@end
